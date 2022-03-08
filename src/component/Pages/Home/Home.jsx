dea5c0d3b8541186853ad37fad9233b48dfa2699
Thomas Buscemi <Thomasbuscemi124@gmail.com>
2022-03-07 22:48:43 -0500

    added a 3rd card

--- src/component/Pages/Home/Home.jsx
+++ src/component/Pages/Home/Home.jsx
@@ -17,6 +17,10 @@ export default function Home() {
                 <h1>2</h1>
                 <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae autem vero consequuntur ea modi tempora sapiente repellendus voluptatem reiciendis fugiat sunt amet temporibus veritatis, animi eius voluptates! Provident, nisi corporis?</h2>
               </div>
+              <div className='flex-cards card-3'>
+                <h1>3</h1>
+                <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi consequuntur dolorem quam officia, accusantium fuga consectetur iure. Dolores commodi sequi aut voluptatibus est vel iusto veniam. Quae inventore fugiat eaque!</h2>
+              </div>
             </div>
         </div>
     </div>
