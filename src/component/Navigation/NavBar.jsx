05d44605414b9b5d3264680d3b57b840ae3b5a6b
Thomas Buscemi <Thomasbuscemi124@gmail.com>
2022-03-07 22:25:24 -0500

    setting up the pages to get started with the flex box example

--- src/component/Navigation/NavBar.jsx
+++ src/component/Navigation/NavBar.jsx
@@ -5,8 +5,8 @@ import { useNavigate } from 'react-router-dom'
 
 export default function NavBar() {
     return (
-        <div>
-            <div>
+        <div className='nav-wrapper'>
+            <div className='nav-cont'>
                 <NavLink className='text-links' to="/">Home</NavLink>
             </div>
         </div>
--- src/component/Pages/Home/Home.jsx
+++ src/component/Pages/Home/Home.jsx
@@ -3,8 +3,15 @@ import "./style/Home.css"
 import NavBar from '../../Navigation/NavBar'
 export default function Home() {
   return (
-    <div>
+    <div >
+      <div>
         <NavBar/>
+      </div>
+        <div className='flex-wrapper'>
+            <div className='flex-simple-container'>
+              <h1>flex</h1>
+            </div>
+        </div>
     </div>
   )
 }
