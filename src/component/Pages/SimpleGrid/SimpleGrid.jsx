20ca19596e62affd97e43f6f219bb29cf4fdc7cd
Thomas Buscemi <Thomasbuscemi124@gmail.com>
2022-03-08 17:27:57 -0500

    got the grid working and adding some coments explaing things abouyt grid. it is also responsive.

--- src/component/Pages/SimpleGrid/SimpleGrid.jsx
+++ src/component/Pages/SimpleGrid/SimpleGrid.jsx
@@ -1,10 +1,63 @@
 import React from 'react'
 import NavBar from '../../Navigation/NavBar'
+import './style/SimpleGrid.css'
 
 export default function SimpleGrid() {
   return (
     <div>
+      <div>
         <NavBar/>
+      </div>
+      <div className='grid-wrapper'>
+        <div className='grid-cards card-1' >
+          <h1>1</h1>
+          <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore cupiditate, mollitia, rerum error eum numquam quis aliquam quasi quos harum assumenda laborum nulla debitis eveniet quisquam, modi ex dolores exercitationem?</h2>
+        </div>
+        <div className='grid-cards card-2'>
+          <h1>2</h1>
+          <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore cupiditate, mollitia, rerum error eum numquam quis aliquam quasi quos harum assumenda laborum nulla debitis eveniet quisquam, modi ex dolores exercitationem?</h2>
+        </div>
+        <div className='grid-cards card-3'>
+          <h1>3</h1>
+          <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore cupiditate, mollitia, rerum error eum numquam quis aliquam quasi quos harum assumenda laborum nulla debitis eveniet quisquam, modi ex dolores exercitationem?</h2>
+        </div>
+        <div className='grid-cards card-4' >
+          <h1>4</h1>
+          <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore cupiditate, mollitia, rerum error eum numquam quis aliquam quasi quos harum assumenda laborum nulla debitis eveniet quisquam, modi ex dolores exercitationem?</h2>
+        </div>
+        <div className='grid-cards card-5'>
+          <h1>5</h1>
+          <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore cupiditate, mollitia, rerum error eum numquam quis aliquam quasi quos harum assumenda laborum nulla debitis eveniet quisquam, modi ex dolores exercitationem?</h2>
+        </div>
+        <div className='grid-cards card-6'>
+          <h1>6</h1>
+          <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore cupiditate, mollitia, rerum error eum numquam quis aliquam quasi quos harum assumenda laborum nulla debitis eveniet quisquam, modi ex dolores exercitationem?</h2>
+        </div>
+        <div className='grid-cards card-1' >
+          <h1>7</h1>
+          <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore cupiditate, mollitia, rerum error eum numquam quis aliquam quasi quos harum assumenda laborum nulla debitis eveniet quisquam, modi ex dolores exercitationem?</h2>
+        </div>
+        <div className='grid-cards card-2'>
+          <h1>8</h1>
+          <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore cupiditate, mollitia, rerum error eum numquam quis aliquam quasi quos harum assumenda laborum nulla debitis eveniet quisquam, modi ex dolores exercitationem?</h2>
+        </div>
+        <div className='grid-cards card-3'>
+          <h1>9</h1>
+          <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore cupiditate, mollitia, rerum error eum numquam quis aliquam quasi quos harum assumenda laborum nulla debitis eveniet quisquam, modi ex dolores exercitationem?</h2>
+        </div>
+        <div className='grid-cards card-4' >
+          <h1>10</h1>
+          <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore cupiditate, mollitia, rerum error eum numquam quis aliquam quasi quos harum assumenda laborum nulla debitis eveniet quisquam, modi ex dolores exercitationem?</h2>
+        </div>
+        <div className='grid-cards card-5'>
+          <h1>11</h1>
+          <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore cupiditate, mollitia, rerum error eum numquam quis aliquam quasi quos harum assumenda laborum nulla debitis eveniet quisquam, modi ex dolores exercitationem?</h2>
+        </div>
+        <div className='grid-cards card-6'>
+          <h1>12</h1>
+          <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore cupiditate, mollitia, rerum error eum numquam quis aliquam quasi quos harum assumenda laborum nulla debitis eveniet quisquam, modi ex dolores exercitationem?</h2>
+        </div>
+      </div>
     </div>
   )
 }
