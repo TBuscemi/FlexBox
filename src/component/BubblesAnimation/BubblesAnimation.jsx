c89fb66bf85dad3400bacecb767a2c302ad4f41c
Thomas Buscemi <Thomasbuscemi124@gmail.com>
2022-03-12 18:15:37 -0500

    went in and broke up all the componets on the home page/ working on a trasue hunt idea and started wrtting a function when you land on a div or when off it

---
+++ src/component/BubblesAnimation/BubblesAnimation.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './style/BubblesAnimation.css'
+
+function BubblesAnimation() {
+  return (
+    <div class="bubbles">
+      <div class="bubble bubble-sm"></div>
+      <div class="bubble bubble-sm1"></div>
+      <div class="bubble bubble-sm2"></div>
+      <div class="bubble bubble-sm3"></div>
+      <div class="bubble bubble-sm4"></div>
+      <div class="bubble bubble-sm5"></div>
+      <div class="bubble bubble-sm6"></div>
+      <div class="bubble bubble-md"></div>
+      <div class="bubble bubble-md1"></div>
+    </div>
+  )
+}
+
+export default BubblesAnimation
