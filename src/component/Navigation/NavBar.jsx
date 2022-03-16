0cc807452615b19b8848e3ef64db483386915ea9
Thomas Buscemi <ThomasBuscemi124@gmail.com>
2022-03-16 01:57:04 -0400

    ran abuild/ made the about me section and get it in the grid where i wanted it/ made a ham at smaller sizes for the nav bar/ added a new svg to the projext for teach and fixed some width sizes so the new ham nav would work correctly.

--- src/component/Navigation/NavBar.jsx
+++ src/component/Navigation/NavBar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './style/NavBar.css'
 import { NavLink } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
+import Hamburger from './Hamburger/Hamburger'
 
 export default function NavBar() {
     return (
@@ -10,12 +11,13 @@ export default function NavBar() {
         <div>
           <h2 className='my-name'>TB.u,s.c.e,m.i</h2>
         </div>
-        <div className='nav-links'>
-          <NavLink className='text-links' to="/">Home</NavLink>
-          <NavLink className='text-links' to="/simpleflex">Simple Flex</NavLink>
-          <NavLink className='text-links' to="/simplegrid">Simple Grid</NavLink>
+          <div className='nav-links'>
+            <NavLink className='text-links' to="/">Home</NavLink>
+            <NavLink className='text-links' to="/simpleflex">Simple Flex</NavLink>
+            <NavLink className='text-links' to="/simplegrid">Simple Grid</NavLink>
+          </div>
+          <Hamburger/>
         </div>
-      </div>
     </div>
   )
 }
