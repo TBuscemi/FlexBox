000f55df6884246003e9fcd311a3d7ca775025d6
Thomas Buscemi <ThomasBuscemi124@gmail.com>
2022-03-17 01:26:42 -0400

    made test fider to mess with things before i go live with it

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router,Route,Routes} from "react-router-dom";
 import Home from "./component/Pages/Home/Home";
 import SimpleFlex from './component/Pages/SimpleFlex/SimpleFlex';
 import SimpleGrid from './component/Pages/SimpleGrid/SimpleGrid';
+import Test from './component/Test/Test';
 function App() {
     return (
         <>
@@ -10,6 +11,7 @@ function App() {
                 <Route  path="/" element={<Home/>}/>    
                 <Route  path="/simpleflex" element={<SimpleFlex/>}/>    
                 <Route  path="/simpleGrid" element={<SimpleGrid/>}/>    
+                <Route  path="/TEST" element={<Test/>}/>    
             </Routes>
         </>
     );
