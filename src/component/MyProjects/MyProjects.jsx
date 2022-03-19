63aef1317fe5bf6d044dd1f6dd1d68e0d855dadb
Thomas Buscemi <ThomasBuscemi124@gmail.com>
2022-03-18 20:21:05 -0400

    got the project cards layed out and now i have to add the details!

--- src/component/MyProjects/MyProjects.jsx
+++ src/component/MyProjects/MyProjects.jsx
@@ -5,17 +5,88 @@ function MyProjects() {
   return (
     <div className='my-project-wrapper'>
       <div className='my-project-container'>
+        <div className='project-cards flex-project-cards-1'>
+          <div className='project-card-title'>
+            <h1>TITLE</h1>
+          </div>
+          <div className='project-card-body'>
+            <svg className='icon-project' stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M108.777 256.058c.286 56.565 13.983 101.827 24.942 133.585C85.56 317.753 30.9 187.787 120.5 112.147c-42.56-4.31-92.374 25.796-95.78 71.213h-.19c-1.068 14.052 2.58 29.756 11.746 46.133C16.312 284.78 15.33 345.708 49.6 433.393c19.18 49.048 70.854 62.702 120.752 53.94 36.346-6.4 70.19-22.667 93.294-59.754 40.158-64.48 59.99-105.418 101.79-135.42 46.526-19.777 97.54-3.25 123.88 22.454-7.476-57.55-70.396-111.51-120.465-56.763-41.325 7.66-69.025 17.52-115.722 51.235 19.65-38.758 56.327-84.913 79.13-112.415 33.693-41.608 102.853-56.82 138.524-48.106-31.99-45.34-127.917-59.59-149.615 12.53-42.008 19.263-94.897 60.85-129.908 102.997 11.58-52.748 33.18-117.786 54.347-155.208 33.825-36.553 83.77-35.932 114.623-26.564-28.904-46.596-121.244-70.12-138.495 12.993-43.11 33.08-87.248 100.11-112.956 160.748z"></path></svg>            
+            <h2 className='border-above-tech'>Technologies</h2>
+            <p>Java script, Django, Python, Html, Css, React.js, Scss, 3rd party libraries</p>
+            <h2>Descriptions</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium, itaque ipsam. In eum magnam, harum sed, ipsa eius perspiciatis doloribus sequi adipisci delectus debitis? Temporibus autem minus facere nostrum inventore.</p>
+          </div>
+          <div className='project-card-footer'>
+            <a href="https://github.com/TBuscemi/" target='_blank'><i class="fa-brands fa-github fab"></i></a>
+            <a>
+              <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-window-fullscreen fab" viewBox="0 0 16 16">
+                <path d="M3 3.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0Zm1.5 0a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0Zm1 .5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1Z"/>
+                <path d="M.5 1a.5.5 0 0 0-.5.5v13a.5.5 0 0 0 .5.5h15a.5.5 0 0 0 .5-.5v-13a.5.5 0 0 0-.5-.5H.5ZM1 5V2h14v3H1Zm0 1h14v8H1V6Z"/>
+              </svg>
+            </a>
+          </div>
+        </div>
         <div className='project-cards'>
           <div className='project-card-title'>
             <h1>TITLE</h1>
           </div>
           <div className='project-card-body'>
-          <svg className='icon-project' stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M108.777 256.058c.286 56.565 13.983 101.827 24.942 133.585C85.56 317.753 30.9 187.787 120.5 112.147c-42.56-4.31-92.374 25.796-95.78 71.213h-.19c-1.068 14.052 2.58 29.756 11.746 46.133C16.312 284.78 15.33 345.708 49.6 433.393c19.18 49.048 70.854 62.702 120.752 53.94 36.346-6.4 70.19-22.667 93.294-59.754 40.158-64.48 59.99-105.418 101.79-135.42 46.526-19.777 97.54-3.25 123.88 22.454-7.476-57.55-70.396-111.51-120.465-56.763-41.325 7.66-69.025 17.52-115.722 51.235 19.65-38.758 56.327-84.913 79.13-112.415 33.693-41.608 102.853-56.82 138.524-48.106-31.99-45.34-127.917-59.59-149.615 12.53-42.008 19.263-94.897 60.85-129.908 102.997 11.58-52.748 33.18-117.786 54.347-155.208 33.825-36.553 83.77-35.932 114.623-26.564-28.904-46.596-121.244-70.12-138.495 12.993-43.11 33.08-87.248 100.11-112.956 160.748z"></path></svg>            
-          <p>tech</p>
-          <p>des</p>
+            <svg className='icon-project' stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M108.777 256.058c.286 56.565 13.983 101.827 24.942 133.585C85.56 317.753 30.9 187.787 120.5 112.147c-42.56-4.31-92.374 25.796-95.78 71.213h-.19c-1.068 14.052 2.58 29.756 11.746 46.133C16.312 284.78 15.33 345.708 49.6 433.393c19.18 49.048 70.854 62.702 120.752 53.94 36.346-6.4 70.19-22.667 93.294-59.754 40.158-64.48 59.99-105.418 101.79-135.42 46.526-19.777 97.54-3.25 123.88 22.454-7.476-57.55-70.396-111.51-120.465-56.763-41.325 7.66-69.025 17.52-115.722 51.235 19.65-38.758 56.327-84.913 79.13-112.415 33.693-41.608 102.853-56.82 138.524-48.106-31.99-45.34-127.917-59.59-149.615 12.53-42.008 19.263-94.897 60.85-129.908 102.997 11.58-52.748 33.18-117.786 54.347-155.208 33.825-36.553 83.77-35.932 114.623-26.564-28.904-46.596-121.244-70.12-138.495 12.993-43.11 33.08-87.248 100.11-112.956 160.748z"></path></svg>            
+            <h2 className='border-above-tech'>Technologies</h2>
+            <p>Java script, Django, Python, Html, Css, React.js, Scss, 3rd party libraries</p>
+            <h2>Descriptions</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium, itaque ipsam. In eum magnam, harum sed, ipsa eius perspiciatis doloribus sequi adipisci delectus debitis? Temporibus autem minus facere nostrum inventore.</p>
           </div>
           <div className='project-card-footer'>
-            <h1>footer</h1>
+            <a href="https://github.com/TBuscemi/" target='_blank'><i class="fa-brands fa-github fab"></i></a>
+            <a>
+              <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-window-fullscreen fab" viewBox="0 0 16 16">
+                <path d="M3 3.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0Zm1.5 0a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0Zm1 .5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1Z"/>
+                <path d="M.5 1a.5.5 0 0 0-.5.5v13a.5.5 0 0 0 .5.5h15a.5.5 0 0 0 .5-.5v-13a.5.5 0 0 0-.5-.5H.5ZM1 5V2h14v3H1Zm0 1h14v8H1V6Z"/>
+              </svg>
+            </a>
+          </div>
+        </div>
+        <div className='project-cards'>
+          <div className='project-card-title'>
+            <h1>TITLE</h1>
+          </div>
+          <div className='project-card-body'>
+            <svg className='icon-project' stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M108.777 256.058c.286 56.565 13.983 101.827 24.942 133.585C85.56 317.753 30.9 187.787 120.5 112.147c-42.56-4.31-92.374 25.796-95.78 71.213h-.19c-1.068 14.052 2.58 29.756 11.746 46.133C16.312 284.78 15.33 345.708 49.6 433.393c19.18 49.048 70.854 62.702 120.752 53.94 36.346-6.4 70.19-22.667 93.294-59.754 40.158-64.48 59.99-105.418 101.79-135.42 46.526-19.777 97.54-3.25 123.88 22.454-7.476-57.55-70.396-111.51-120.465-56.763-41.325 7.66-69.025 17.52-115.722 51.235 19.65-38.758 56.327-84.913 79.13-112.415 33.693-41.608 102.853-56.82 138.524-48.106-31.99-45.34-127.917-59.59-149.615 12.53-42.008 19.263-94.897 60.85-129.908 102.997 11.58-52.748 33.18-117.786 54.347-155.208 33.825-36.553 83.77-35.932 114.623-26.564-28.904-46.596-121.244-70.12-138.495 12.993-43.11 33.08-87.248 100.11-112.956 160.748z"></path></svg>            
+            <h2 className='border-above-tech'>Technologies</h2>
+            <p>Java script, Django, Python, Html, Css, React.js, Scss, 3rd party libraries</p>
+            <h2>Descriptions</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora impedit maiores tenetur non fugiat quaerat quisquam error possimus id earum iste saepe, qui ducimus modi officia asperiores repudiandae. Laudantium, reiciendis! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium, itaque ipsam. In eum magnam, harum sed, ipsa eius perspiciatis doloribus sequi adipisci delectus debitis? Temporibus autem minus facere nostrum inventore.</p>
+          </div>
+          <div className='project-card-footer'>
+            <a href="https://github.com/TBuscemi/" target='_blank'><i class="fa-brands fa-github fab"></i></a>
+            <a>
+              <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-window-fullscreen fab" viewBox="0 0 16 16">
+                <path d="M3 3.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0Zm1.5 0a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0Zm1 .5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1Z"/>
+                <path d="M.5 1a.5.5 0 0 0-.5.5v13a.5.5 0 0 0 .5.5h15a.5.5 0 0 0 .5-.5v-13a.5.5 0 0 0-.5-.5H.5ZM1 5V2h14v3H1Zm0 1h14v8H1V6Z"/>
+              </svg>
+            </a>
+          </div>
+        </div>
+        <div className='project-cards'>
+          <div className='project-card-title'>
+            <h1>TITLE</h1>
+          </div>
+          <div className='project-card-body'>
+            <svg className='icon-project' stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M108.777 256.058c.286 56.565 13.983 101.827 24.942 133.585C85.56 317.753 30.9 187.787 120.5 112.147c-42.56-4.31-92.374 25.796-95.78 71.213h-.19c-1.068 14.052 2.58 29.756 11.746 46.133C16.312 284.78 15.33 345.708 49.6 433.393c19.18 49.048 70.854 62.702 120.752 53.94 36.346-6.4 70.19-22.667 93.294-59.754 40.158-64.48 59.99-105.418 101.79-135.42 46.526-19.777 97.54-3.25 123.88 22.454-7.476-57.55-70.396-111.51-120.465-56.763-41.325 7.66-69.025 17.52-115.722 51.235 19.65-38.758 56.327-84.913 79.13-112.415 33.693-41.608 102.853-56.82 138.524-48.106-31.99-45.34-127.917-59.59-149.615 12.53-42.008 19.263-94.897 60.85-129.908 102.997 11.58-52.748 33.18-117.786 54.347-155.208 33.825-36.553 83.77-35.932 114.623-26.564-28.904-46.596-121.244-70.12-138.495 12.993-43.11 33.08-87.248 100.11-112.956 160.748z"></path></svg>            
+            <h2 className='border-above-tech'>Technologies</h2>
+            <p>Java script, Django, Python, Html, Css, React.js, Scss, 3rd party libraries</p>
+            <h2>Descriptions</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium, itaque ipsam. In eum magnam, harum sed, ipsa eius perspiciatis doloribus sequi adipisci delectus debitis? Temporibus autem minus facere nostrum inventore.</p>
+          </div>
+          <div className='project-card-footer'>
+            <a href="https://github.com/TBuscemi/" target='_blank'><i class="fa-brands fa-github fab"></i></a>
+            <a>
+              <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-window-fullscreen fab" viewBox="0 0 16 16">
+                <path d="M3 3.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0Zm1.5 0a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0Zm1 .5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1Z"/>
+                <path d="M.5 1a.5.5 0 0 0-.5.5v13a.5.5 0 0 0 .5.5h15a.5.5 0 0 0 .5-.5v-13a.5.5 0 0 0-.5-.5H.5ZM1 5V2h14v3H1Zm0 1h14v8H1V6Z"/>
+              </svg>
+            </a>
           </div>
         </div>
       </div>
